f3bdf5d82cf9da825704636f576dd1e5ec1781d9
rohithvudayana <rohithvudayana46@gmail.com>
2023-11-14 23:03:25 +0530

    5th

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import Main from "./pages/Main";
-import {BrowseRouter as Router, Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import { Suspense, lazy } from 'react';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, Navigate} from 'react-router-dom'
 import { routes } from './routes/routes'
+import SuspenseLoader from './components/common/SuspenseLoader';
+
+const ErrorComponent = lazy(() => import('./components/common/ErrorComponent'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path={routes.main.path } element = {<routes.main.element/>} />
+      <Route path={routes.main.path} element={<Navigate to={`${routes.emails.path}/inbox`} />} />
+      <Route path={routes.main.path} element={<routes.main.element />} >
+        <Route path={`${routes.emails.path}/:type`} element={<routes.emails.element />} errorElement={<ErrorComponent />} />
+        <Route path={routes.view.path} element={<routes.view.element />} errorElement={<ErrorComponent />} />
+      </Route>
+
+      <Route path={routes.invalid.path} element={<Navigate to={`${routes.emails.path}/inbox`} />} />
     </Route>
   )
 )
 
 function App() {
   return (
-    <RouterProvider router = {router}/>
+    <Suspense fallback={<SuspenseLoader />}>
+      {/* <DataProvider> */}
+        <RouterProvider router={router} />
+      {/* </DataProvider> */}
+    </Suspense>
   );
 }
 
